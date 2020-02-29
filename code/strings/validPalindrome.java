0d294cb94899ceb9d6988bc969442ac80e4a413b
Hilal Tarakci <hlltarakci@gmail.com>
2020-02-29 18:59:40 +0300

    add solution

---
+++ code/strings/validPalindrome.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/valid-palindrome/
+
+class Solution {
+    /*
+        time: O(n)
+        space: O(1)
+    */
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length()-1;
+        
+        while(left < right) {
+            if(!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+                continue;
+            }
+            
+            if(!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+                continue;
+            }
+            
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)))
+                return false;
+            
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+    
+}
