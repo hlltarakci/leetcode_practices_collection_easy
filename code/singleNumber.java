4eefaf0f1e753d57b1217e05bad8305416f472ad
Hilal Tarakci <hlltarakci@gmail.com>
2020-02-29 10:34:00 +0300

    add solution

---
+++ code/singleNumber.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/single-number/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        
+        for(int num: nums) result ^= num;
+        
+        return result;
+    }
+}
