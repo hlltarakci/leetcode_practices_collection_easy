ab8de391760c50e3b28304066de55967c77b4c7b
Hilal Tarakci <hlltarakci@gmail.com>
2020-02-29 11:16:13 +0300

    add solution

---
+++ code/strings/reverseString.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/reverse-string/
+
+class Solution {
+    /*
+        time: O(n)
+        space: O(1)
+    */
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length-1;
+        
+        while(left < right) {
+            char temp = s[left];
+            s[left++] = s[right];
+            s[right--] = temp;
+        }
+    }
+}
