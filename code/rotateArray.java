bdfb1a933d4dbc00ad80c88650007139e0f60797
Hilal Tarakci <hlltarakci@gmail.com>
2020-02-29 10:31:16 +0300

    add solution

---
+++ code/rotateArray.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/rotate-array/
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if(nums.length == 0) return;
+        
+        k = k % nums.length;
+        reverseBetween(nums, 0, nums.length - 1);
+        reverseBetween(nums, 0, k-1);
+        reverseBetween(nums, k, nums.length - 1);
+        
+    }
+    
+    private void reverseBetween(int[] nums, int left, int right) {
+        while(left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
