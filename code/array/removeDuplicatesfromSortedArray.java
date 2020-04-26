5d36308e85d9b8f0851bd9989762672a50f5bdc7
Hilal Tarakci <hlltarakci@gmail.com>
2020-04-26 23:32:54 +0300

    add complexity

--- code/array/removeDuplicatesfromSortedArray.java
+++ code/array/removeDuplicatesfromSortedArray.java
@@ -1,6 +1,10 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
 class Solution {
+    /*
+        time: O(n)
+        space: O(1)
+    */
     public int removeDuplicates(int[] nums) {
         if(nums == null || nums.length == 0) return 0;
         
