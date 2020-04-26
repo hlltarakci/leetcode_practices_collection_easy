fa7c8611e3b9f434156f752457da57e2e447c7ae
Hilal Tarakci <hlltarakci@gmail.com>
2020-04-26 23:37:08 +0300

    add complexity

--- code/array/singleNumber.java
+++ code/array/singleNumber.java
@@ -1,6 +1,10 @@
 // https://leetcode.com/problems/single-number/
 
 class Solution {
+    /*
+        time: O(n)
+        space: O(1)
+    */
     public int singleNumber(int[] nums) {
         int result = 0;
         
