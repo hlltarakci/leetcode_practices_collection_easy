c49314cb636a72b57d0582cea3eaacf022c85d9a
Hilal Tarakci <hlltarakci@gmail.com>
2020-04-27 00:17:26 +0300

    add cleaner impl

--- code/others/validParentheses.java
+++ code/others/validParentheses.java
@@ -7,19 +7,17 @@ class Solution {
         space: O(n) -- stack usage -- worst case all open pars
     */
     public boolean isValid(String s) {
-        ArrayList<Character> openPars = new ArrayList<>(Arrays.asList('(', '[', '{'));
-        ArrayList<Character> closePars = new ArrayList<>(Arrays.asList(')', ']', '}'));
+        String openPars = "({[";
+        String closePars = ")}]";
         
         Stack<Character> stack = new Stack<>();
         
         for(char c: s.toCharArray()) {
-            if(openPars.contains(c)) stack.push(c);
+            if(openPars.indexOf(c) >= 0) stack.push(c);
             else {
                 if(stack.isEmpty()) return false;
                 
-                char poppedChar = stack.pop();
-                if(openPars.indexOf(poppedChar) != 
-                    closePars.indexOf(c)) return false;
+                if(openPars.indexOf(stack.pop()) != closePars.indexOf(c)) return false;
             }
         }
         
