0a67c78739db2d133351d5b3d7e9371d396d49f8
Hilal Tarakci <hlltarakci@gmail.com>
2020-04-27 00:08:36 +0300

    add more readable solution

--- code/dynamicProgramming/houseRobber.java
+++ code/dynamicProgramming/houseRobber.java
@@ -6,13 +6,20 @@ class Solution {
         space: O(1)
     */
     public int rob(int[] nums) {
-        int prevMax = 0, currMax = 0;
-        for(int num: nums) {
-            int temp = currMax;
-            currMax = Math.max(prevMax + num, currMax);
-            prevMax = temp;
+        // no houses
+        if(nums.length == 0) return 0;
+        // only one house
+        if(nums.length == 1) return nums[0];
+        // only two houses
+        if(nums.length == 2) return Math.max(nums[0], nums[1]);
+        
+        int maxForPrev = Math.max(nums[0], nums[1]), maxForPrevPrev = nums[0];
+        for(int i=2; i<nums.length; i++) {
+            int max = Math.max(maxForPrevPrev + nums[i], maxForPrev);
+            maxForPrevPrev = maxForPrev;
+            maxForPrev = max;
         }
         
-        return currMax;
+        return maxForPrev;
     }
 }
