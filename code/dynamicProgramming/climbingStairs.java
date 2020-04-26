824a7d18f92bdb9ef5856141512fd46f424279cd
Hilal Tarakci <hlltarakci@gmail.com>
2020-04-27 00:06:35 +0300

    add more performant impl

--- code/dynamicProgramming/climbingStairs.java
+++ code/dynamicProgramming/climbingStairs.java
@@ -3,16 +3,18 @@
 class Solution {
     /*
         time: O(n)
-        space: O(n)
+        space: O(1)
     */
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
+        if(n <= 3) return n;
         
-        for(int i=2; i <= n ; i++) 
-            dp[i] = dp[i-1] + dp[i-2];
+        int prev1 = 1, prev2 = 2;
+        for(int i=3; i<=n; i++) {
+            int val = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = val;
+        }
         
-        return dp[n];
+        return prev2;
     }
 }
