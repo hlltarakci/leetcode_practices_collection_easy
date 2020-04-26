9b9fe1c651a7bfc0c786e6e76e816c28b6e0b1d6
Hilal Tarakci <hlltarakci@gmail.com>
2020-04-27 00:12:09 +0300

    add more performant solution

--- code/math/countPrimes.java
+++ code/math/countPrimes.java
@@ -1,22 +1,34 @@
 // https://leetcode.com/problems/count-primes/
 
 class Solution {
-    public int countPrimes(int n) {
-        boolean[] isPrime = new boolean[n];
-        for(int i=2; i<n; i++) isPrime[i] = true;
-        
-        for(int i=2; i*i < n; i++) {
-            if(!isPrime[i]) continue;
-            
-            for(int j = i*i; j < n ; j += i)
-                isPrime[j] = false;
-        }
+   public int countPrimes(int n) {
+        boolean[] isNotPrimeArray = new boolean[n+1];
+        if(n < 2) return 0;
         
         int count = 0;
-        for(int i=2; i<n; i++) 
-            if(isPrime[i]) count++;
+        for(int i=2; i<n; i++) {
+            if(isNotPrimeArray[i]) continue;
+            boolean isThisPrime = isPrime(i);
+            if(isThisPrime) {
+                count++;
+                fillForward(isNotPrimeArray, i);
+            }
+        }
         
         return count;
+    }
+    
+    public boolean isPrime(int n) {
+        for(int i=2; i*i<n; i++) {
+            if( n % i == 0) return false;
+        }
+        
+        return true;
+    }
+    
+    private void fillForward(boolean arr[], int k) {
+        for(int i=k; i<arr.length; i+=k) 
+            arr[i] = true;
     }
     
  }
