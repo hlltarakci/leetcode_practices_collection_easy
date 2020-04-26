3779f88438dd48281843689435d639b228e5b7ec
Hilal Tarakci <hlltarakci@gmail.com>
2020-04-26 23:44:35 +0300

    add complexity

--- code/array/moveZeroes.java
+++ code/array/moveZeroes.java
@@ -1,6 +1,10 @@
 // https://leetcode.com/problems/move-zeroes/
 
 class Solution {
+    /*
+        time: O(n)
+        space: O(1)
+    */
     public void moveZeroes(int[] nums) {
         int noZeroIndex = -1;
         for(int i=0; i < nums.length; i++) {
